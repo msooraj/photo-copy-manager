03e03669d96afd69832c52f0037f20cac69a4783
Sooraj M <msooraj@gmail.com>
2017-04-08 21:34:29 -0400

    removed commented code.

--- PhotoManager/src/com/util/PhotoManager.java
+++ PhotoManager/src/com/util/PhotoManager.java
@@ -1,6 +1,5 @@
 package com.util;
 
-import java.io.File;
 import java.io.IOException;
 import java.nio.file.DirectoryStream;
 import java.nio.file.FileSystems;
@@ -19,9 +18,7 @@ import java.time.format.DateTimeFormatter;
 
 import com.drew.imaging.ImageMetadataReader;
 import com.drew.imaging.ImageProcessingException;
-import com.drew.metadata.Directory;
 import com.drew.metadata.Metadata;
-import com.drew.metadata.Tag;
 import com.drew.metadata.exif.ExifSubIFDDirectory;
 
 public class PhotoManager {
@@ -142,34 +139,5 @@ public class PhotoManager {
     Files.createDirectories(Paths.get(destPath + "/" + year));
     return year;
   }
-
-  private static void readMetaData() throws ImageProcessingException, IOException {
-    Metadata metadata = ImageMetadataReader.readMetadata(new File("C:/Sooraj/Learning/IMG_4270.JPG"));
-    ExifSubIFDDirectory directory = metadata.getFirstDirectoryOfType(ExifSubIFDDirectory.class);
-    String dateStr = directory.getString(ExifSubIFDDirectory.TAG_DATETIME_ORIGINAL);
-
-    DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy:MM:dd HH:mm:ss");
-    LocalDateTime formatDateTime = LocalDateTime.parse(dateStr, formatter);
-    ZonedDateTime zdt = formatDateTime.atZone(ZoneId.of("America/New_York"));
-    long millis = zdt.toInstant().toEpochMilli();
-    FileTime ft = FileTime.fromMillis(millis);
-    SimpleDateFormat dfdate = new SimpleDateFormat("yyyy:MM:dd HH:mm:ss");
-    String dateCreated = dfdate.format(ft.toMillis());
-
-    System.out.println(dateStr);
-    System.out.println(formatDateTime);
-    System.out.println(dateCreated);
-  }
-
-  private static void readMetadata() throws ImageProcessingException, IOException {
-
-    Metadata metadata = ImageMetadataReader.readMetadata(new File("C:/Sooraj/Learning/IMG_4270.JPG"));
-    for (Directory directory : metadata.getDirectories()) {
-      for (Tag tag : directory.getTags()) {
-        System.out.println(tag);
-      }
-    }
-  }
-
 
 }
